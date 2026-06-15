b05e36a437a76b8f34105c1857fd92de85288e46
Jordan <jordan.baughman@snhu.edu>
2026-06-15 12:51:54 -0700

    set up game portal

--- Source/GAM415/GAM415Character.h
+++ Source/GAM415/GAM415Character.h
@@ -47,6 +47,10 @@ protected:
 	virtual void BeginPlay();
 
 public:
+
+
+	UPROPERTY(EditAnywhere)
+		bool isTeleporting;
 		
 	/** Look Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
